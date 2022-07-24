5211f1a769c90d0de13bfd940e7e52226f44a5c0
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-24 15:30:01 +1000

    update test

--- tests.R
+++ tests.R
@@ -2,7 +2,11 @@
 
 AI <- function(warrior, memory = NULL, level_state = NULL) {
   action <- "walk"
+  # warrior.walk()
   list(action = action, memory = memory)
+  # warrior.walk(direction = "right", memory = memory) # this is how it should be called, as we need directions
+  # warrior.walk(direction = "right")
 }
+
 
 play_warrior(AI)
