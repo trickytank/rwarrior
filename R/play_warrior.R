ccd5f7c6451189ace6a72beb31a9e923aedb1833
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-24 11:52:18 +1000

    updates to play_warrior()

--- R/play_warrior.R
+++ R/play_warrior.R
@@ -1,6 +1,17 @@
 # Play the warrior game
 
-
+#' Play R Warrior
+#'
+#' @param ai AI function to control your warrior
+#' @param y Level number.
+#' @return A logical that is TRUE on successfully getting to the exit
+#' @export
+#' @examples
+#' AI <- AI <- function(warrior, level_state) {
+#'   warrior <- warrior.walk()
+#'   warrior
+#' }
+#' play_warrior(AI, level = 1)
 play_warrior <- function(ai, level = 1) {
   w <- warrior_new()
   level_state <- level_01
@@ -9,29 +20,35 @@ play_warrior <- function(ai, level = 1) {
   turns_left <- 100L
   alive <- TRUE
   while(!complete) {
-    message("Turns left: " turns_left)
+    message("Turns left: ", turns_left)
     result <- warrior_turn(ai, w, level_state)
-    w <- result$x
-    level_state <- result$w
+    w <- result$w
+    level_state <- result$level_state
     message_level_state(level_state)
     at_exit <- result$at_exit
     alive <- result$alive
     turns_left <- turns_left - 1L
     if(!alive) {
       message("Your warrior died.")
-      return(NULL)
+      return(FALSE)
     }
     if(turns_left == 0) {
       message("Sorry, you have run out of time.")
-      return(NULL)
+      return(FALSE)
     }
     message("-----------------------------------")
   }
   message("Success, you have completed level ", level, ".")
-  return(NULL)
+  return(TRUE)
 }
 
 warrior_turn <- function(ai, w, level_state) {
-  w <- ai(w)
-  list(w, level_state, at_exit, alive)
+  ai_result <- ai(w, level_state)
+  action_result <- UseMethod(ai_result$action, ..., level_state)
+  if(action_result$at_exit) {
+    return(action_result)
+  }
+  #TODO: enemy attacks
+  return(action_result)
+  #list(w, level_state, at_exit, alive)
 }
