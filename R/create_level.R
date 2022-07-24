dd16ce33052568f498f6726853890abb50c41c20
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-24 11:52:03 +1000

    level creater updated

--- R/create_level.R
+++ R/create_level.R
@@ -6,6 +6,6 @@
 # '>' : exit
 # 's' : sludge
 create_level <- function(level_string) {
-  x <- strplit(level_string, "")[[1]]
-  structure(x, "level")
+  x <- strsplit(level_string, "")[[1]]
+  structure(x, class = "level")
 }
