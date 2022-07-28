d13b98b8dee362fccdb7ea2ff7353f120923b079
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-28 21:16:43 +1000

    Use the new level state as a string

--- R/message_level_state.R
+++ R/message_level_state.R
@@ -1,5 +1,4 @@
 message_level_state <- function(level_state) {
-  message(" ", rep("-", length(level_state)), " ")
-  message("|", paste0(level_state, collapse = ""), "|")
-  message(" ", rep("-", length(level_state)), " ")
+  message(level_state$state)
 }
+
