6ca635b392e3cb876b3b75c41cddd642a9309f6f
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-28 21:17:06 +1000

    level_state function

---
+++ R/level_state.R
@@ -0,0 +1,10 @@
+Level_state <- R6Class(
+  "Level_state",
+  public = list(
+    state = NULL,
+    initialize = function(level_map) {
+      self$state <- level_map
+      invisible(self$state)
+    }
+  )
+)
