f71cf7fb7358ceb1c0fbce7cb896501f1af980e5
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-28 21:16:06 +1000

    Warrior_action class

---
+++ R/warrior_action.R
@@ -0,0 +1,28 @@
+# Definitions for the S3 class warrior.
+
+Warrior_action <- R6Class(
+  "Warrior_action",
+  public = list(
+    action = NULL,
+    feel_right = NULL,
+    feel_left = NULL,
+    direction = NULL,
+    initialize = function(health, feel_left = NA, feel_right = NA) {
+      private$health <- health
+      self$feel_left <- feel_left
+      self$feel_right <- feel_right
+    },
+    walk = function(direction = "+") {
+      if(!is.null(private$action)) {
+        stop("A warrior action has already been defined.")
+      }
+      self$action <- "walk"
+      self$direction <- direction
+      invisible(self)
+    }
+  ),
+  private = list(
+    health = NULL
+  )
+)
+
