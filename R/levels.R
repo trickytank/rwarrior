c4288347f0feff8fd2692519600a405e131d1020
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-31 03:40:42 +1000

    Added level 2

--- R/levels.R
+++ R/levels.R
@@ -9,4 +9,12 @@ levels[[1]] <- list(
   ace_score = 10,
   methods = c("walk")
 )
-levels[[2]] <- matrix(c("@", " ", " ", "s", " ", " ", " ", ">"), nrow = 1, byrow = TRUE)
+
+levels[[2]] <- list(
+  description = "It is too dark to see anything, but you smell sludge nearby.",
+  matrix(c("@", " ", " ", " ", "s", " ", " ", ">"), nrow = 1, byrow = TRUE),
+  tip = "Use warrior$feel$empty to see if there is anything in front of you, and warrior$attack() to fight it.\nRemember, you can only do one actionper turn.",
+  time_bonus = 20,
+  ace_score = 26,
+  methods = c("walk", "attack", "feel$empty")
+)
