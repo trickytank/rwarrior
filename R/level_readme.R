f4e1b012f130646dd674e7692175a8f969b16016
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-31 03:36:49 +1000

    Updated level_readme()

--- R/level_readme.R
+++ R/level_readme.R
@@ -17,6 +17,12 @@ method_description <- function(method) {
     cat('    - "left"\n')
     cat('    - "up"\n')
     cat('    - "down"\n')
+  } else if (method == "feel$empty") {
+    cat('- warrior$feel$empty')
+    cat('  Returns TRUE if the space in front is empty or the stairs, and FALSE otherwise.')
+  } else if (method == "attack") {
+    cat('- warrior$attack(direction = "forward")')
+    cat('  Attack the space in the given direction (foward by default).')
   } else {
     stop("Internal error, level has undefined method. (Please report this bug).")
   }
