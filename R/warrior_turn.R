0fbd28b6b76af38408ffe03661c860e0ebe7d265
Rick Tankard <trickytank@users.noreply.github.com>
2022-07-31 09:49:04 +1000

    added fight action

--- R/warrior_turn.R
+++ R/warrior_turn.R
@@ -38,9 +38,33 @@ warrior_turn <- function(w, health, level_state, warrior_name) {
     } else {
       stop("Invalid direction specified in walk()")
     }
+  } else if (w$action == "fight") {
+    if(!is.na(pmatch(w$direction, "forward"))) {
+      direc <- "forward"
+      offset <- 1L
+    } else if (!is.na(pmatch(w$direction, "backward"))) {
+      direc <- "backward"
+      offset <- -1L
+    } else {
+      stop("Invalid direction specified in fight()")
+    }
+    if(map[y, x + offset] %in% names(enemy_types)) {
+      enemy_short <- map[y, x + offset]
+      enemy <- enemy_types[enemy_short]
+      level_state$hp[y, x + 1L] <- level_state$hp[y, x + offset] - attack_power
+      message(warrior_name, "attacks", direc, "and hits", paste0(enemy, "."))
+      message(enemy, "takes", attack_power, "damage,", level_state$hp[y, x + offset], "health power left.")
+      health <- health - enemy_power[enemy_short]
+      message(enemy, "attacks", direc,"and hits", paste0(warrior_name, "."))
+      message(warrior_name, "takes", enemy_power[enemy_short], "damage,", health, "health power left.")
+    } else {
+      message(warrior_name, "attacks forward and hits nothing.")
+    }
+  } else {
+    stop("Invalid warrior action (this is a bug).")
   }
   level_state$map <- map
-  at_exit
+  list(at_exit = at_exit, health = health)
 }
 
 swap_positions <- function(M, x1, y1, x2, y2) {
@@ -49,5 +73,9 @@ swap_positions <- function(M, x1, y1, x2, y2) {
   M[y2, x2] <- temp
   M
 }
+
+enemy_types <- c("s" = "Sludge", "S" = "Super Sludge")
+enemy_power <- c("s" = 3, "S" = NA)
+attack_power <- 5
 
 
