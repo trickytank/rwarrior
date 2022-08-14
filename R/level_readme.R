fc170b31d959795536f0e0971da44299aeb05be8
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-14 20:08:10 +1000

    redo newlines

--- R/level_readme.R
+++ R/level_readme.R
@@ -1,9 +1,9 @@
 #' export
 level_readme <- function(level = 1) {
-  cat("Level", level, "\n\n")
+  cat("Level", level, "\n")
   cat(Level_state$new(levels[[level]])$ascii, "\n")
   cat(levels[[level]]$description, "\n")
-  cat("Tip:", levels[[level]]$tip, "\n")
+  cat("Tip:", levels[[level]]$tip, "\n\n")
   method_description(levels[[level]]$warrior)
 }
 
@@ -23,13 +23,11 @@ method_description <- function(warrior) {
     cat('  Attack the space in the given direction (foward by default).\n')
   }
   if(warrior$rest) {
-    cat('- warrior$rest()')
+    cat('- warrior$rest()\n')
     cat('  Rest and heal 10% of the your warrior\'s health.')
   }
   if(warrior$health) {
     cat('- warrior$health\n')
     cat('  Returns the health of the warrior, up to 20HP.')
-  } else {
-    stop("Internal error, level has undefined method. (Please report this bug).")
   }
 }
