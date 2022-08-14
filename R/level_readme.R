66ef0f5819eb9e015830dae195e8345711a9a391
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-14 19:27:38 +1000

    Use warrior abilities to decide actions

--- R/level_readme.R
+++ R/level_readme.R
@@ -4,30 +4,29 @@ level_readme <- function(level = 1) {
   cat(Level_state$new(levels[[level]])$ascii, "\n")
   cat(levels[[level]]$description, "\n")
   cat("Tip:", levels[[level]]$tip, "\n")
-  for(meth in levels[[level]]$methods) {
-    cat("\n")
-    method_description(meth)
-  }
+  method_description(levels[[level]]$warrior)
 }
 
-method_description <- function(method) {
-  if(method == "walk") {
-    cat("- warrior$walk(direction = \"right\")\n")
+method_description <- function(warrior) {
+  if(warrior$walk) {
+    cat("- warrior$walk(direction = \"forward\")\n")
     cat("  Move the warrior in the given direction, any of:\n")
-    cat('    - "right"\n')
-    cat('    - "left"\n')
-    cat('    - "up"\n')
-    cat('    - "down"\n')
-  } else if (method == "feel$empty") {
+    cat('    - "forward"\n')
+    cat('    - "backward"\n')
+  }
+  if(warrior$feel) {
     cat('- warrior$feel$empty\n')
     cat('  Returns TRUE if the space in front is empty or the stairs, and FALSE otherwise.\n')
-  } else if (method == "attack") {
+  }
+  if(warrior$attack) {
     cat('- warrior$attack(direction = "forward")\n')
     cat('  Attack the space in the given direction (foward by default).\n')
-  } else if(method == "rest") {
+  }
+  if(warrior$rest) {
     cat('- warrior$rest()')
     cat('  Rest and heal 10% of the your warrior\'s health.')
-  } else if(method == "health") {
+  }
+  if(warrior$health) {
     cat('- warrior$health\n')
     cat('  Returns the health of the warrior, up to 20HP.')
   } else {
