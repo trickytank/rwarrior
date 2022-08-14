dfb376edac6d602df2d8a7bda127a52a5ad175cf
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-14 19:22:48 +1000

    NPC and warrior class

--- R/characters.R
+++ R/characters.R
@@ -1,8 +1,9 @@
-NPC_TYPE <- R6Class(
-  "NPC_TYPE",
+NPC <- R6Class(
+  "NPC",
   public = list(
     name = NULL,
     hp = NULL,
+    max_hp = NULL,
     symbol = NULL,
     attack_power = NULL,
     shoot_power = NULL,
@@ -13,11 +14,13 @@ NPC_TYPE <- R6Class(
     x = NULL,
     y = NULL,
     direction = NULL,
-    initialize = function(name, symbol, hp, attack_power = NULL, shoot_power = NULL,
+    direction_default = "west",
+    initialize = function(name, symbol, max_hp, attack_power = NULL, shoot_power = NULL,
                feel = FALSE, look = FALSE, attack = FALSE, shoot = FALSE) {
       self$name <- name
       self$symbol <- symbol
-      self$hp <- hp
+      self$max_hp <- max_hp
+      self$hp <- max_hp
       self$attack_power <- attack_power
       self$shoot_power <- shoot_power
       self$feel <- feel
@@ -25,7 +28,7 @@ NPC_TYPE <- R6Class(
       self$attack <- attack
       self$shoot <- shoot
     },
-    set_loc = function(y, x, direction = "west") {
+    set_loc = function(y, x, direction = self$direction_default) {
       self$y <- y
       self$x <- x
       self$direction <- direction
@@ -34,25 +37,31 @@ NPC_TYPE <- R6Class(
   )
 )
 
-NPC <- R6Class(
-  "NPC",
-  inherit = NPC_TYPE,
+WARRIOR <- R6Class(
+  "WARRIOR",
+  inherit = NPC,
   public = list(
-    x = NULL,
-    y = NULL,
-    direction = NULL,
-    # type is a NPC_TYPE object
-    initialize = function(type, x, y, direction = "west") {
+    name = "Warrior",
+    symbol = "@",
+    hp = 20L,
+    attack_power = 5L,
+    walk = NULL,
+    rest = NULL,
+    health = NULL,
+    direction_default = "east",
+    initialize = function(walk = FALSE, feel = FALSE, look = FALSE, attack = FALSE, shoot = FALSE,
+                          rest = FALSE, health = FALSE) {
+      self$walk <- walk
+      self$feel <- feel
+      self$look <- look
+      self$attack <- attack
+      self$shoot <- shoot
+      self$rest <- rest
+      self$health <- health
+    }
 
-    }
-    set_loc = function(y, x, direction = "west") {
-      self$y <- y
-      self$x <- x
-      self$direction <- direction
-    }
   )
 )
-
 
 
 npc_sludge <- NPC_TYPE$new("Sludge", "s", 12L, attack_power = 3L, feel = TRUE, attack = TRUE)
@@ -61,6 +70,6 @@ npc_thick_sludge <- NPC_TYPE$new("Thick Sludge", "S", 24L)
 
 npc_archer <- NPC_TYPE$new("Archer", "a", 7L, shoot_power = 3L, look = TRUE, shoot = TRUE)
 
-x <- npc_sludge$clone()
+x <- npc_sludge$clone()$set_loc(1L, 4L)
 x
 x$hp
