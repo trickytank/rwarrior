f67a1020dbbbf96b00395e2557828b41c4d78392
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-15 23:48:19 +1000

    attack routine fixed

--- R/warrior_turn.R
+++ R/warrior_turn.R
@@ -16,20 +16,19 @@ warrior_turn <- function(w, level_state, warrior_name, sleep = 0) {
   x_subject <- coord$x_subject
   direc <- coord$direc
   if(w$action == "walk") {
-    if(is.na(level_state$return_object(y_subject, x_subject))) {
+    if(is.null(level_state$return_object(y_subject, x_subject))) {
       cat(glue("{warrior_name} walks {direc}."))
       level_state$warrior$y <- y_subject
       level_state$warrior$x <- x_subject
     } else {
       message(glue("{warrior_name is blocked and doesn't move."))
     }
-
   } else if (w$action == "attack") {
     enemy <- level_state$return_object(y_subject, x_subject)
-    if(is.na(enemy)) {
+    if(is.null(enemy)) {
       message(warrior_name, " attacks forward and hits nothing.")
     } else {
-      points <- points + attack_routine(level_state$warrior, enemy, direc, sleep)
+      points <- points + level_state$attack_routine(level_state$warrior, enemy, direc, sleep)
     }
   } else if(w$action == "rest") {
     if(level_state$warrior$hp >= level_state$warrior$max_hp) {
@@ -49,14 +48,15 @@ warrior_turn <- function(w, level_state, warrior_name, sleep = 0) {
   for(enemy in level_state$npcs) {
     if(enemy$attack) {
       # check if they are within range
-      if(npc$feel() == "@") {
+      if(level_state$feel(enemy) == "@") {
         # Do the attacking
-        attack_routine(enemy, level_state$warrior, direc, sleep)
+        level_state$attack_routine(enemy, level_state$warrior, "forward", sleep)
       }
     }
     if(enemy$shoot) {
       # Not implemented
     }
   }
+
   list(points = points)
 }
