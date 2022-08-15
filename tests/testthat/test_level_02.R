5d93400d6111ed23caf9a81eab0d4551b6dd3ebb
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-15 23:48:30 +1000

    reindentation

--- tests/testthat/test_level_02.R
+++ tests/testthat/test_level_02.R
@@ -1,12 +1,13 @@
-expect_true(play_warrior(
-  function(warrior, memory) {
-    if(warrior$feel()$empty) {
-      warrior$walk()
-    } else {
-      warrior$attack()
-    }
-  },
-  sleep = 0, level = 2)
+expect_true(
+  play_warrior(
+    function(warrior, memory) {
+      if(warrior$feel() == " ") {
+        warrior$walk()
+      } else {
+        warrior$attack()
+      }
+    },
+    sleep = 0, level = 2)
 )
 
 # Attacking an empty space should do nothing
