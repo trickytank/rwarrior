965c793e03abd630bbba286c88ff25563299f001
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-15 22:03:08 +1000

    give_coordinates function

---
+++ R/give_coordinates.R
@@ -0,0 +1,26 @@
+give_coordinates <- function(compass, direction, y = 0, x = 0) {
+  if(compass == "east") {
+    x_offset <- 1L
+  }
+  if(compass == "west") {
+    x_offset <- -1L
+  }
+  if(compass == "north") {
+    y_offset <- -1L
+  }
+  if(compass == "south") {
+    y_offset <- 1L
+  }
+  if(!is.na(pmatch(direction, "forward"))) {
+    direc <- "forward"
+  } else if (!is.na(pmatch(direction, "backward"))) {
+    direc <- "backward"
+    x_offset <- x_offset * -1L
+    y_offset <- y_offset * -1L
+  } else {
+    stop("Invalid direction specified.")
+  }
+  y_subject <- y + y_offset
+  x_subject <- x + x_offset
+  list(direc, y_offset, x_offset)
+}
