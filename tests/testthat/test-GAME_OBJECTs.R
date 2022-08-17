2bd010635a5383f463885b44bab4b35130eec155
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-17 23:16:42 +1000

    testing set_loc() method in test-GAME_OBJECTs.R

--- tests/testthat/test-GAME_OBJECTs.R
+++ tests/testthat/test-GAME_OBJECTs.R
@@ -1,6 +1,6 @@
 test_that("GAME_OBJECT R6 class", {
   expect_true(
-    inherits(GAME_OBJECT$new("Wall", "-"),
+    inherits(GAME_OBJECT$new("Wall", "-")$set_loc(0, 0),
              "GAME_OBJECT")
   )
 })
@@ -8,14 +8,14 @@ test_that("GAME_OBJECT R6 class", {
 
 test_that("NPC R6 class", {
   expect_true(
-    inherits(NPC$new("Sludge", "s", 12L, attack_power = 3L, feel = TRUE, attack = TRUE),
+    inherits(NPC$new("Sludge", "s", 12L, attack_power = 3L, feel = TRUE, attack = TRUE)$set_loc(3, 1),
              c("NPC", "GAME_OBJECT"))
   )
 })
 
 test_that("WARRIOR R6 class", {
   expect_true(
-    inherits(WARRIOR$new(feel = TRUE),
+    inherits(WARRIOR$new(feel = TRUE)$set_loc(1,1),
              c("WARRIOR", "NPC", "GAME_OBJECT"))
     )
 })
