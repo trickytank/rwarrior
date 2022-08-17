238284f603b5ee409dc66263f3c5e558d0ff30bd
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-17 22:05:58 +1000

    enemy attack sequence change
    Prepare enemys to attack only if they are within range at the start of the turn.

--- R/warrior_turn.R
+++ R/warrior_turn.R
@@ -9,8 +9,21 @@ warrior_turn <- function(w, level_state, warrior_name, sleep = 0, debug = FALSE)
   x <- level_state$warrior$x
   y <- level_state$warrior$y
   points <- 0
-  x_offset <- 0
-  y_offset <- 0
+  # Prepare for enemies to attack if they are close enough
+  enemys_to_attack <- list()
+  for(enemy in level_state$npcs) {
+    if(enemy$attack) {
+      # check if they are within range
+      if(level_state$feel(enemy) == "@") {
+        # Prepare to attack
+        enemys_to_attack <- c(list(enemy), enemys_to_attack)
+      }
+    }
+    if(enemy$shoot) {
+      # Not implemented
+    }
+  }
+
   if(w$action %in% c("walk", "attack")) {
     coord <- give_coordinates(level_state$warrior$compass, w$direction, y, x)
     y_subject <- coord$y_subject
@@ -31,7 +44,6 @@ warrior_turn <- function(w, level_state, warrior_name, sleep = 0, debug = FALSE)
       message(warrior_name, " attacks forward and hits nothing.")
     } else {
       points <- points + level_state$attack_routine(level_state$warrior, enemy, direc, sleep, debug = debug)
-      message_sleep(sleep, debug)
     }
   } else if(w$action == "rest") {
     if(level_state$warrior$hp >= level_state$warrior$max_hp) {
@@ -47,20 +59,14 @@ warrior_turn <- function(w, level_state, warrior_name, sleep = 0, debug = FALSE)
     stop("Invalid warrior action (this is a bug).")
   }
 
-  # Let enemies attack if they are close enough
-  for(enemy in level_state$npcs) {
-    if(enemy$attack) {
-      # check if they are within range
-      if(level_state$feel(enemy) == "@") {
-        # Do the attacking
-        level_state$attack_routine(enemy, level_state$warrior, "forward", sleep, debug = debug)
-      }
-    }
-    if(enemy$shoot) {
-      # Not implemented
-    }
-  }
   message_sleep(sleep, debug)
+
+  # Let enemies attack if they are close enough
+  for(enemy in enemys_to_attack) {
+    # Do the attacking
+    level_state$attack_routine(enemy, level_state$warrior, "forward", sleep, debug = debug)
+    message_sleep(sleep, debug)
+  }
 
   list(points = points)
 }
