1aa1fb59699e9f431a923a6d0fd7897aaf6c7904
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-21 11:53:41 +1000

    Updated level comments and added level 4

--- R/zzz_levels.R
+++ R/zzz_levels.R
@@ -3,7 +3,9 @@
 levels <- list()
 
 # Level 1
-# "@      >"
+# ----------
+# |@      >|
+# ----------
 levels[[1]] <- list(
   description = "You see before yourself a long hallway with stairs at the end. There is nothing in the way.",
   size = c(1,8),
@@ -15,7 +17,9 @@ levels[[1]] <- list(
   ace_score = 10
 )
 
-# "@   s  >"
+# ----------
+# |@   s  >|
+# ----------
 levels[[2]] <- list(
   description = "It is too dark to see anything, but you smell sludge nearby.",
   size = c(1,8),
@@ -25,11 +29,14 @@ levels[[2]] <- list(
   ),
   stairs = c(1,8),
   tip = "Use warrior$feel$empty to see if there is anything in front of you, and warrior$attack() to fight it.\nRemember, you can only do one actionper turn.",
+  clue = "Add an if/else condition using warrior$feel$empty to decide whether to warrior$attack() or warrior$walk().",
   time_bonus = 20,
   ace_score = 26
 )
 
-# "@ s ss s>"
+# -----------
+# |@ s ss s>|
+# -----------
 levels[[3]] <- list(
   description = "The air feels thicker than before. There must be a horde of sludge.",
   size = c(1,9),
@@ -42,6 +49,27 @@ levels[[3]] <- list(
   ),
   stairs = c(1, 9),
   tip = "Be careful not to die! Use warrior.health to keep an eye on your health, and warrior.rest! to earn 10% of max health back.",
+  clue = "When there is no enemy ahead of you call warrior$rest() until health is full before walking forward.",
   time_bonus = 35,
   ace_score = 71
+)
+
+
+# ---------
+# |@ Sa S>|
+# ---------
+levels[[4]] <- list(
+  description = "You can hear bow strings being stretched.",
+  size = c(1,7),
+  warrior = WARRIOR$new(feel = TRUE, attack = TRUE, health = TRUE, rest = TRUE)$set_loc(1L, 1L),
+  npcs = list(
+    thick_sludge_here(1, 3),
+    archer_here(1, 4),
+    thick_sludge_here(1, 6)
+  ),
+  stairs = c(1, 7),
+  tip = "No new abilities this time, but you must be careful not to rest while taking damage. Save a list called memory with a memory$health item (and return memory in the AI fucntion) and compare it on each turn to see if you're taking damage.",
+  clue = "Set memory$health to your current health at the end of the turn. If this is greater than your current health next turn then you know you're taking damage and shouldn't rest.",
+  time_bonus = 45,
+  ace_score = 90
 )
