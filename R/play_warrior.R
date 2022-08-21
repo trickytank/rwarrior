f839e8de4a24af6d58ffbafb19aee20791806bb0
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-21 20:13:24 +1000

    partial removal of output in testing

--- R/play_warrior.R
+++ R/play_warrior.R
@@ -15,11 +15,11 @@
 #' }
 #' play_warrior(AI, level = 1)
 play_warrior <- function(ai, level = 1, warrior_name = "Fisher", sleep = 0.5) {
-  play_warrior_inbuilt_levels(ai = ai, level = level, warrior_name = warrior_name, sleep = sleep, debug = FALSE)
+  play_warrior_inbuilt_levels(ai = ai, level = level, warrior_name = warrior_name, sleep = sleep, debug = FALSE, output = TRUE)
 }
 
 # For inbuilt levels
-play_warrior_inbuilt_levels <- function(ai, level = 1, warrior_name = "Fisher", sleep = 0, debug = TRUE, output = TRUE) {
+play_warrior_inbuilt_levels <- function(ai, level = 1, warrior_name = "Fisher", sleep = 0, debug = FALSE, output = FALSE) {
   if(level > length(levels)) {
     if(level <= 18) {
       stop("Level ", level, " does not exist, though it is planned for the future.")
@@ -27,12 +27,14 @@ play_warrior_inbuilt_levels <- function(ai, level = 1, warrior_name = "Fisher", 
     stop("Level ", level, " does not exist.")
   }
   game_state <- GAME_STATE$new(levels[[level]])
-  play_warrior_work(ai, game_state, level = level, warrior_name = warrior_name, sleep = sleep, debug = debug, output = output)
+  play_warrior_work(ai, game_state, level = level, warrior_name = warrior_name,
+                    sleep = sleep, debug = debug, output = output)
 }
 
 # The work of the warrior, allowing for custom levels to be used.
 # TODO: remove level, and store time bonus etc. in the GAME_STATE class
-play_warrior_work <- function(ai, game_state, level = NULL, warrior_name = "Fisher", sleep = 0, debug = TRUE, output = TRUE) {
+play_warrior_work <- function(ai, game_state, level = NULL, warrior_name = "Fisher",
+                              sleep = 0, debug = TRUE, output = FALSE) {
   game_state$warrior$name <- warrior_name
   at_stairs <- FALSE
   complete <- FALSE
@@ -41,48 +43,50 @@ play_warrior_work <- function(ai, game_state, level = NULL, warrior_name = "Fish
   level_score <- 0L
   memory <- NULL
   while(!complete) {
-    message("-----------------------------------")
-    cat("- Turn", turn, "-\n")
-    cat(game_state$ascii)
+    if(output) cli_h1("Turn {turn}")
+    if(output) cat(game_state$ascii)
     x <- game_state$x
     y <- game_state$y
     # clone here to prevent tampering the game_state. Doesn't prevent all cheating such as inspecting the entire game_state.
     w <- WARRIOR_ACTION$new(game_state$deep_clone())
     # w is also modified here
     memory <- ai(w, memory)
-    result <- warrior_turn(w, game_state, warrior_name, sleep, debug = debug)
+    result <- warrior_turn(w, game_state, warrior_name, sleep, debug = debug, output = output)
     points <- result$points
 
     level_score <- level_score + points
 
     if(game_state$warrior$hp <= 0) {
-      message(warrior_name, " died.")
+      if(output) cli_text("{warrior_name} died.")
       return(invisible(FALSE))
     }
 
     if(game_state$at_stairs) {
       complete <- TRUE
-      message("-----------------------------------")
-      cat(game_state$ascii)
-      message("Success, you have found the stairs.")
-      time_bonus <- max(0, game_state$level_time_bonus - turn)
-      clear_bonus <- game_state$level_clear_bonus
-      total_score <- time_bonus + level_score + clear_bonus
-      cat("Level Score:", level_score, "\n",
-          "Time Bonus:", time_bonus, "\n",
-          "Clear Bonus:", clear_bonus, "\n",
-          "Total Score:", total_score, "\n")
-      # 0 8 2 10
-      if(is.null(level)) {
-        cli_text("Congratulations, you have completed this custom level.")
-      } else {
-        if(level + 1 > length(levels)) {
-          message("Congratulations, you have completed all the levels of R Warrior and reached the precious Hex.")
-          if(level <= 18) {
-            message(18 - level, " more levels are planned to be ported from Ruby Warrior.")
-          }
+      if(output) {
+        cli_h1("Found stairs")
+        cat(game_state$ascii)
+        cli_text("Success, you have found the stairs.")
+        time_bonus <- max(0, game_state$level_time_bonus - turn)
+        clear_bonus <- game_state$level_clear_bonus
+        total_score <- time_bonus + level_score + clear_bonus
+        cli_text("Level Score: {level_score}")
+        cat(glue("Time Bonus:  {time_bonus}"), "\n")
+        cli_text("Clear Bonus: {clear_bonus}")
+        cli_text("Level Score: {level_score}")
+        cli_text("Total Score: {total_score}")
+        # 0 8 2 10
+        if(is.null(level)) {
+          cli_text("Congratulations, you have completed this custom level.")
         } else {
-          message(paste0("See the readme for the next level of the tower with level_readme(", level + 1, ")"))
+          if(level + 1 > length(levels)) {
+            cli_text("Congratulations, you have completed all the levels of R Warrior and reached the precious Hex.")
+            if(level <= 18) {
+              cli_text("{18 - level} more levels are planned to be ported from Ruby Warrior.")
+            }
+          } else {
+            cli_text("See the readme for the next level of the tower with level_readme({level + 1})")
+          }
         }
       }
       return(invisible(TRUE))
