3e81ff299aee2b00103047d1d9b19ffa450a7510
Rick Tankard <trickytank@users.noreply.github.com>
2022-08-29 14:16:29 +1000

    link to runner temp path

--- tests/testthat/test_play_epic_S_grade.R
+++ tests/testthat/test_play_epic_S_grade.R
@@ -1,5 +1,5 @@
-if(Sys.getenv("SECRET_FILE") != "") {
-  path_to_ai <- Sys.getenv("SECRET_FILE")
+if(Sys.getenv("RUNNER_TEMP") != "") {
+  path_to_ai <- file.path(Sys.getenv("SECRET_FILE"), "test_play_epic_S_grade_AI.R")
 } else {
   path_to_ai <- "../../../Rwarrior-private/tests/testthat/test_play_epic_S_grade_AI.R"
 }
