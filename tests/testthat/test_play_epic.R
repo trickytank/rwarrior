207e33dee6a4ff783ea39edaf13fc28580429652
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-01 21:20:35 +1000

    test epic for A rank

---
+++ tests/testthat/test_play_epic.R
@@ -0,0 +1,85 @@
+if(Sys.getenv("RUNNER_TEMP") != "") {
+  path_to_ai <- file.path(Sys.getenv("RUNNER_TEMP"), "test_play_epic_S_grade_AI.R")
+} else {
+  path_to_ai <- "../../../Rwarrior-private/tests/testthat/test_play_epic_S_grade_AI.R"
+}
+skip_if_no_epic_ai <- function() {
+  if (!file.exists(path_to_ai)) {
+    skip("Advanced AI not available for play_epic().")
+  }
+}
+
+test_that("Epic tower S rank", {
+  skip_if_no_epic_ai()
+  source(path_to_ai)
+  expect_match(purrr::quietly(play_epic)(
+    AI_epic_great,
+    tower = c("beginner"),
+    warrior_name = "Fisher",
+    level_output = TRUE,
+    sleep = 0)$messages,
+    "Overall grade: A", all = FALSE # want to make this S grade
+  )
+})
+
+AI_09 <- function(warrior, memory) {
+  if(is.null(memory)) {
+    # give initial values when memory is NULL
+    memory <- list(previous_health = 20, forward = TRUE)
+  }
+  if(warrior$feel()$wall) {
+    warrior$pivot()
+  } else {
+    if(memory$forward) {
+      if(warrior$feel()$empty) {
+        ahead <- warrior$look()
+        if(ahead[[2]]$enemy || (ahead[[2]]$empty && ahead[[3]]$enemy)) {
+          if(warrior$health < memory$previous_health) {
+            warrior$walk()
+          } else {
+            warrior$shoot()
+          }
+        } else {
+          if(warrior$health < memory$previous_health) {
+            if(warrior$health < 7) {
+              warrior$walk("backward")
+              memory$forward <- FALSE
+            } else {
+              warrior$walk()
+            }
+          } else if (warrior$health < 20) {
+            warrior$rest()
+          } else {
+            warrior$walk()
+          }
+        }
+      } else if (warrior$feel()$captive) {
+        warrior$rescue()
+      } else {
+        warrior$attack()
+      }
+    } else {
+      if(warrior$feel("backward")$captive) {
+        warrior$rescue("backward")
+      } else if (warrior$feel("backward")$wall) {
+        memory$forward = TRUE
+        warrior$rest()
+      } else {
+        warrior$walk("backward")
+      }
+    }
+  }
+  memory$previous_health <- warrior$health
+  memory
+}
+
+test_that("Epic tower A rank", {
+  expect_match(purrr::quietly(play_epic)(
+    AI_09,
+    tower = c("beginner"),
+    warrior_name = "Fisher",
+    level_output = TRUE,
+    sleep = 0)$messages,
+    "Overall grade: A", all = FALSE
+  )
+})
