cee2ace51a8e15a9163f622d3564d7808c386c30
Rick Tankard <trickytank@users.noreply.github.com>
2022-09-02 23:22:47 +1000

    correcting tests
    with play_warrior() and WARRIOR_ACTION

--- tests/testthat/test-play_warrior.R
+++ tests/testthat/test-play_warrior.R
@@ -1,6 +1,6 @@
 test_that("play_warrior() errors", {
   expect_error(play_warrior(sum, level = 10000), "Level 10000 does not exist in the beginner tower")
   expect_match(purrr::quietly(play_warrior)(function(warrior, memory) { warrior$walk() }, level = 1, sleep = "prompt")$messages, "")
-  expect_error(play_warrior(function(){}, tower = "fake"), "Unknown tower")
+  expect_error(play_warrior_inbuilt_levels(function(){}, tower = "fake"), "Unknown tower")
   expect_error(play_warrior(function(){}, sleep = "fake"), "Sleep is not correctly specified")
 })
